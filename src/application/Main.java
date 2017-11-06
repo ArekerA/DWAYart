0a1571a59b446f2fdf0f5569371411b75391b9c4
Bartek1817 <Bartek1817@interia.pl>
2017-11-06 23:18:48 +0100

    gitgnore

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
+//
 
 public class Main extends Application implements Runnable {
 	@Override
